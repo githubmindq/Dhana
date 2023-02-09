76a860d568846c2404159fbdb06a445de6979b2b
dhana <dhana@DESKTOP-KPKAHGG>
2023-02-09 16:30:57 +0530

    New project OHRMS checked in

---
+++ OHRMS-New/src/main/java/Mindq/OHRMS_New/App.java
@@ -0,0 +1,13 @@
+package Mindq.OHRMS_New;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
